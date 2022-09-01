c2e370d0f86787639dc4df615b4ee5bbf3a0a324
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-09-01 11:27:54 +0200

    Calculator

---
+++ Calculator/Calculator.Test.Unit/TestsCalculator.cs
@@ -0,0 +1,84 @@
+using Calculator;
+
+namespace Calculator.Test.Unit
+{
+    public class TestsCalculator
+    {
+        private CalculatorClass uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new CalculatorClass();
+        }
+
+        [TestCase(0, 0)]
+
+        [TestCase(2, 5)]
+        [TestCase(3, -1)]
+        [TestCase(-4, 5)]
+        [TestCase(-3, -2)]
+        [TestCase(2.4, 5.2)]
+        [TestCase(3.3, -1.2)]
+        [TestCase(-4.2, 5.6)]
+        [TestCase(-3.1, -2.9)]
+        public void AddTwoNumbersTogether(double param1, double param2)
+        {
+            double TestNumber = uut.Add(param1, param2);
+            Assert.That(TestNumber, Is.EqualTo(param1 + param2));
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(0, 5)]
+        [TestCase(5, 0)]
+        [TestCase(2, 5)]
+        [TestCase(3, -1)]
+        [TestCase(-4, 5)]
+        [TestCase(-3, -2)]
+        [TestCase(2.4, 5.2)]
+        [TestCase(3.3, -1.2)]
+        [TestCase(-4.2, 5.6)]
+        [TestCase(-3.1, -2.9)]
+        public void MinusTwoNumberTogether(double param1, double param2)
+        {
+
+            double TestNumber = uut.Subtract(param1, param2);
+            Assert.That(TestNumber, Is.EqualTo(param1 - param2));
+        }
+
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(0, 5)]
+        [TestCase(5, 0)]
+        [TestCase(2, 5)]
+        [TestCase(3, -1)]
+        [TestCase(-4, 5)]
+        [TestCase(-3, -2)]
+        [TestCase(2.4, 5.2)]
+        [TestCase(3.3, -1.2)]
+        [TestCase(-4.2, 5.6)]
+        [TestCase(-3.1, -2.9)]
+        public double MultiplyTwoNumberTogether(double param1, double param2)
+        {
+            return uut.Multiply(param1, param2);
+            //double TestNumber = uut.Multiply(param1, param2);
+            //Assert.That(TestNumber, Is.EqualTo(param1 * param2));
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(0, 5)]
+        [TestCase(5, 0)]
+        [TestCase(2, 5)]
+        [TestCase(3, -1)]
+        [TestCase(-4, 5)]
+        [TestCase(-3, -2)]
+        [TestCase(2.4, 5.2)]
+        [TestCase(3.3, -1.2)]
+        [TestCase(-4.2, 5.6)]
+        [TestCase(-3.1, -2.9)]
+        public void PowerOfTwoNumberTogether(double param1, double param2)
+        {
+            double TestNumber = uut.Power(param1, param2);
+            Assert.That(TestNumber, Is.EqualTo(Math.Pow(param1, param2)));
+        }
+    }
+}
---
+++ Calculator/Calculator/CalculatorClass.cs
@@ -0,0 +1,24 @@
+﻿namespace Calculator
+{
+    public class CalculatorClass
+    {
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        { return a - b; }
+
+        public double Multiply(double a, double b)
+        {
+            return b * a;
+        }
+
+        public double Power(double x, double exp)
+        {
+            return Math.Pow(x, exp);
+        }
+    }
+}

0cc8cc682437e2c781a6957f36ec0ab96a5ca74f
Mythanhthile <93250566+Mythanhthile@users.noreply.github.com>
2022-09-02 11:39:07 +0200

    unittest udvidelse

--- Calculator/Calculator.Test.Unit/TestsCalculator.cs
+++ Calculator/Calculator.Test.Unit/TestsCalculator.cs
@@ -80,5 +80,45 @@ namespace Calculator.Test.Unit
             double TestNumber = uut.Power(param1, param2);
             Assert.That(TestNumber, Is.EqualTo(Math.Pow(param1, param2)));
         }
+
+        [TestCase(0, 0)]
+        [TestCase(0, 5)]
+        [TestCase(5, 0)]
+        [TestCase(2, 5)]
+        [TestCase(3, -1)]
+        [TestCase(-4, 5)]
+        [TestCase(-3, -2)]
+        [TestCase(2.4, 5.2)]
+        [TestCase(3.3, -1.2)]
+        [TestCase(-4.2, 5.6)]
+        [TestCase(-3.1, -2.9)]
+        public void DivideOfTwoNumbers(double param1, double param2)
+        {
+            double TestNumber = uut.Divide(param1, param2);
+            Assert.That(TestNumber, Is.EqualTo(Math.DivRem(param1, param2)));
+        }
+
+        //public void DivideWithZero(double param1, double param2)
+        //{
+        //    double TestNumber = uut.Divide(param1, param2);
+        //    Assert.That(TestNumber, Is.EqualTo(Math.DivRem(param1, param2)));
+        //}
+
+        public void AccumulatorSuccess()
+        {
+     
+            Assert.That()
+
+        }
+
+        public void AccumulatorFail()
+        {
+
+        }
+
+        public void AccumulatorCleared( )
+        {
+            Assert.Zero()
+        }
     }
 }

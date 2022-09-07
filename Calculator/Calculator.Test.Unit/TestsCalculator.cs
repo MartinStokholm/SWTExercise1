902a4c4eafc129d0e89b1cbca415453775b81985
BjarkeKarlsen <37935745+BjarkeKarlsen@users.noreply.github.com>
2022-09-07 16:50:37 +0200

    Update TestsCalculator.cs

--- Calculator/Calculator.Test.Unit/TestsCalculator.cs
+++ Calculator/Calculator.Test.Unit/TestsCalculator.cs
@@ -24,9 +24,9 @@ namespace Calculator.Test.Unit
         [TestCase(0, ExpectedResult = 0)]
         [TestCase(-4.5, ExpectedResult = -4.5)]
         [DefaultFloatingPointTolerance(0.05)]
-        public double AddTwoNumbersTogether(double param1)
+        public double AddTwoNumbersTogether(double param)
         {
-            return uut.Add(param1);
+            return uut.Add(param);
 
         }
 
@@ -34,34 +34,28 @@ namespace Calculator.Test.Unit
         [TestCase(9.5, ExpectedResult = -9.5)]
         [TestCase(5.5, ExpectedResult = -5.5)]
         [DefaultFloatingPointTolerance(0.05)]
-        public double MinusTwoNumberTogether(double param1)
+        public double MinusTwoNumberTogether(double param)
         {
 
-            return uut.Subtract(param1);
+            return uut.Subtract(param);
         }
 
-        //[TestCase(0, ExpectedResult = 0)]
-        //[TestCase(0, ExpectedResult = 0)]
-        //[TestCase(2.4, ExpectedResult = 12.48)]
-        //[TestCase(-4.2, ExpectedResult = -23.52)]
-        //[TestCase(-3.1, ExpectedResult = 8.99)]
-        //[DefaultFloatingPointTolerance(0.05)]
+        [TestCase(0, ExpectedResult = 0)]
+        [TestCase(2.4, ExpectedResult = 0)]
+        [TestCase(-4.2, ExpectedResult = 0)]
+        [DefaultFloatingPointTolerance(0.05)]
+        public double MultiplyTwoNumberTogether(double param) {
+            return uut.Multiply(param);
+        }
 
-        //public double MultiplyTwoNumberTogether(double param1)
-        //{
-        //    return uut.Multiply(param1);
-        //}
-
-        //[TestCase(0, ExpectedResult = 1)]
-        //[TestCase(0, ExpectedResult = 0)]
-        //[TestCase(2.4, ExpectedResult = 94.863349)]
-        //[TestCase(-4.2, ExpectedResult = Double.NaN)]
-        //[TestCase(-3.1, ExpectedResult = Double.NaN)]
-        //[DefaultFloatingPointTolerance(0.05)]
-        //public double PowerOfTwoNumberTogether(double param1)
-        //{
-        //    return uut.Power(param1);
-        //}
+        [TestCase(0, ExpectedResult = 1)]
+        [TestCase(2.4, ExpectedResult = 0)]
+        [TestCase(-4.2, ExpectedResult = Double.PositiveInfinity)]
+        [TestCase(-3.1, ExpectedResult = Double.PositiveInfinity)]
+        [DefaultFloatingPointTolerance(0.05)]
+        public double PowerOfTwoNumberTogether(double param) {
+            return uut.Power(param);
+        }
 
         //[TestCase(0, 0)]
         //[TestCase(0, 5)]
@@ -74,19 +68,19 @@ namespace Calculator.Test.Unit
         //[TestCase(3.3, -1.2)]
         //[TestCase(-4.2, 5.6)]
         //[TestCase(-3.1, -2.9)]
-        //public void DivideOfTwoNumbers(double param1)
+        //public void DivideOfTwoNumbers(double param)
         //{
-        //    double TestNumber = uut.Divide(param1);
-        //    Assert.That(TestNumber, Is.EqualTo(Math.DivRem(param1)));
+        //    double TestNumber = uut.Divide(param);
+        //    Assert.That(TestNumber, Is.EqualTo(Math.DivRem(param)));
         //}
 
-        //public void DivideWithZero(double param1, double param2)
+        //public void DivideWithZero(double param, double param2)
         //{
-        //    double TestNumber = uut.Divide(param1, param2);
-        //    Assert.That(TestNumber, Is.EqualTo(Math.DivRem(param1, param2)));
+        //    double TestNumber = uut.Divide(param, param2);
+        //    Assert.That(TestNumber, Is.EqualTo(Math.DivRem(param, param2)));
         //}
 
-        //public void accumulatorsuccess()
+        //public void Accumulatorsuccess()
         //{
 
         //    assert.that();
@@ -98,6 +92,6 @@ namespace Calculator.Test.Unit
 
         //}
 
-        
+
     }
 }
